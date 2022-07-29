2bc5316055ffceb68c4d08a9b9acab1b0a4ac6f5
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-29 20:55:41 +0300

    add

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Company from './components/company/Company';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Right from './components/right/Right';
+import Services from './components/services/Services';
 
 function App() {
   return (
-    <div className='container'>
+    <div className='App'>
       <Navbar />
       <Hero />
       <Right />
       <Company />
+      <Services />
     </div>
   );
 }
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Services.module.css';
+
+const Services = () => {
+  return (
+    <div className={styles.services}>
+      <div className={styles.title}>
+        <h1>
+          Our <span>Services</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
