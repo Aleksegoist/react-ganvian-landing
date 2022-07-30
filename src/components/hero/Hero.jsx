73c5e15281b4252b07bae9a6578fd660d94d9c00
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-30 10:49:10 +0300

    add

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import styles from './Hero.module.css';
 
 const Hero = () => {
-  return <div className={styles.hero}>Hero</div>;
+  return (
+    <div className={styles.hero}>
+      <div className={styles.hero_text}>
+        <div className={styles.title}>
+          <h1></h1>
+        </div>
+        <div className={styles.desc}>
+          <p></p>
+        </div>
+        <div className={styles.btn}>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
