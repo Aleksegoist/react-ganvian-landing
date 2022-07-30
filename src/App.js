045c6e41a4ee10e99aaa3909749a3c5d7eb54012
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-30 15:52:52 +0300

    add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Company from './components/company/Company';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+import Services from './components/services/Services';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Company />
+      <Services />
     </div>
   );
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
 import styles from './Services.module.css';
+import Decor_Services from '../../assets/decoration_services.png';
+import Service1 from '../../assets/service1.png';
+import Service2_IOS from '../../assets/service2_ios.png';
+import Service3_Android from '../../assets/service3_android.png';
+import Service4_Laptop from '../../assets/service4_laptop.png';
 
 const Services = () => {
-  return <div className={styles.services}></div>;
+  return (
+    <div className={styles.services}>
+      <div className={styles.image}>
+        <img src={Decor_Services} alt='/' />
+      </div>
+      <div>
+        <h2 className='header'>
+          Our <span>Services</span>
+        </h2>
+      </div>
+      <div className={styles.blocks}>
+        <div className={styles.box}>
+          <div className={styles.content}>
+            <div className={styles.circle}>
+              <img src={Service1} alt='/' />
+            </div>
+            <p>Product Management</p>
+          </div>
+        </div>
+        <div className={styles.box}>
+          <div className={styles.content}>
+            <div className={styles.circle} style={{ background: '#F1F1F1' }}>
+              <img src={Service2_IOS} alt='/' />
+            </div>
+            <p>IOS Developement</p>
+          </div>
+        </div>
+        <div className={styles.box}>
+          <div className={styles.content}>
+            <div className={styles.circle} style={{ background: '#DAFFE2' }}>
+              <img src={Service3_Android} alt='/' />
+            </div>
+            <p>Android Developement</p>
+          </div>
+        </div>
+        <div className={styles.box}>
+          <div className={styles.content}>
+            <div className={styles.circle} style={{ background: '#D1E9FF' }}>
+              <img src={Service4_Laptop} alt='/' />
+            </div>
+            <p>Software Developement</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
