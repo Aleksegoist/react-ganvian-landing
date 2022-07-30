0751767fe4be2d5d4dea9d7e838f02f073c2c65b
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-30 12:13:20 +0300

    add

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import styles from './Hero.module.css';
+import Hero_people from '../../assets/people_right.png';
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.hero_text}>
-        <div className={styles.title}>
-          <h1></h1>
+      <div>
+        <h1 className='title'>
+          A company focused on innovation in <span>software creation.</span>
+        </h1>
+        <p className='desc'>
+          We always seek the best way to help our customers with good agility.
+        </p>
+        <div className='btn'>
+          <button>Free Consultation</button>
+          <button>Reed More</button>
         </div>
-        <div className={styles.desc}>
-          <p></p>
-        </div>
-        <div className={styles.btn}>
-          <button></button>
-          <button></button>
-        </div>
+      </div>
+      <div className={styles.hero_image}>
+        <img src={Hero_people} alt='/' />
       </div>
     </div>
   );
